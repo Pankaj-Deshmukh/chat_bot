d554ba9fc9336ef1dc40089fa717eeb0223550b1
kalyanppc <kalyanppc@gmail.com>
2024-01-28 22:31:51 +0530

    added a custom hook for searchMainAnswer content

---
+++ Frontend/src/hooks/searchBody.js
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+const axios = require('axios');
+
+export function useAnswer(searchTerm){
+      const [answer, setAnswer] = useState("");
+      useEffect(async ()=>{
+            const ans = await axios.post("http://localhost:3002/search",{
+                  textInput: searchTerm
+            })
+            setAnswer(ans);
+      },[searchTerm])
+      return answer;
+}
